8db595b9546ddc2894ea0661a3c2fcd7d1245a73
Leslie Lee <leslie@krossover.com>
2016-08-04 14:13:29 -0400

    add first set of packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,30 @@
+ // compile all .js files except the ones coming from node_modules
+var es6ify = require('es6ify').configure(/^(?!.*node_modules)+.+\.js$/);
+
 module.exports = function(grunt){
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
-  })
+
+    /* Load all grunt tasks. */
+    require('load-grunt-tasks')(grunt);
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        browserify:{
+            options:{
+                transform:[es6ify],
+                bundleOptions:{
+                    debug: true
+                },
+                browserifyOptions:{
+                    noParse: ['./public/app.js']
+                }
+            },
+            files:{
+                'public/app.js' : ['client/app.js']
+            }
+        },
+        watch:{
+            files: ['client/**/*.js'],
+            tasks:['browserify']
+        }
+    });
 }
